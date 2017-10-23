98634a38edfbefaf883a6f942b946e33310310ed
Jason <jason@ipedis.com>
2017-10-23 14:26:32 +0400

    Request Response RedirectRoute

--- src/PlatformBundle/Controller/AdvertController.php
+++ src/PlatformBundle/Controller/AdvertController.php
@@ -3,7 +3,9 @@
 namespace PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class AdvertController extends Controller
@@ -27,6 +29,41 @@ class AdvertController extends Controller
         }
 
         return $this->render('PlatformBundle:Advert:index.html.twig',array('msg'=>$msg));
+    }
+
+    public function testAction($id, Request $request){
+        $tag = $request->request->get('tag');
+
+        return new Response("l'affichage de l'id ".$id." avec le tag: ".$tag);
+    }
+
+    public function test2Action($id){
+        // On crée la réponse sans lui donner de contenu pour le moment
+
+        $response = new Response();
+
+        // On définit le contenu
+
+        $response->setContent("Ceci est une page d'erreur 404");
+
+
+        // On définit le code HTTP à « Not Found » (erreur 404)
+
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+
+
+        // On retourne la réponse
+
+        return $response;
+    }
+
+    public function test3Action(){
+
+//       $url = $this->get('router')->generate('app_platform_index');
+//
+//       return $this->redirect($url);
+        
+        return $this->redirectToRoute('app_platform_index');
     }
 
 
